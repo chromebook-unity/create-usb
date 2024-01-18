41ae7de1fc7b915b0c4347f3f063bc87046fe686
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-01-17 18:48:27 -0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -31,6 +31,8 @@ else
     exit
 fi
 
+echo "Extending rootfs..."
+sudo bash /scripts/extend-rootfs.sh
 echo ""
 echo "Setting password for user linux..."
 passwd
@@ -46,7 +48,7 @@ sudo apt purge snapd --autoremove -y
 sudo apt install gnome-software -y
 echo ""
 echo "Installing Brave Browser..."
-sudo apt install curl
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
