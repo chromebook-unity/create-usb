b5f381199afa2f0d5282c427ea4d224595738f1b
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-01-18 09:38:19 -0800

    Update second.sh

--- second.sh
+++ second.sh
@@ -2,10 +2,12 @@
 
 echo "Welcome to the second part of the Ubuntu Unity Conversion Script!"
 echo "Removing XFCE and some remnants of GNOME..."
-sudo apt-get purge xfconf xfce4-utils xfwm4 xfce4-session xfdesktop4 exo-utils xfce4-panel xfce4-terminal gnome-system-tools thunar libxfce4ui* *xfce* --autoremove -y
+sudo apt purge xfconf xfce4-utils xfwm4 xfce4-session xfdesktop4 exo-utils xfce4-panel xfce4-terminal gnome-system-tools thunar libxfce4ui* *xfce* --autoremove -y
 sudo apt purge gdm3 gnome-sudoku gnome-shell --autoremove -y
-sudo apt upgrade -y
+sudo apt upgrade -y --autoremove
 sudo apt install gnome-software -y
+echo "Removing GIMP to free up space..."
+sudo apt purge gimp --autoremove -y
 echo "The second part of the Ubuntu Unity Conversion Script has finished..."
 echo "Rebooting in 10 seconds... (Press CTRL+C to cancel reboot)"
 sleep 10
