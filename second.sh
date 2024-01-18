1b9433d2e21d540a0116fb2b30b222379b371a32
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-01-17 19:20:37 -0800

    Update second.sh

--- second.sh
+++ second.sh
@@ -3,7 +3,7 @@
 echo "Welcome to the second part of the Ubuntu Unity Conversion Script!"
 echo "Removing XFCE and some remnants of GNOME..."
 sudo apt-get purge xfconf xfce4-utils xfwm4 xfce4-session xfdesktop4 exo-utils xfce4-panel xfce4-terminal gnome-system-tools thunar libxfce4ui* *xfce* --autoremove -y
-sudo apt purge gnome* --autoremove -y
+sudo apt purge gdm3 gnome-sudoku gnome-shell --autoremove -y
 sudo apt upgrade -y
 sudo apt install gnome-software -y
 echo "The second part of the Ubuntu Unity Conversion Script has finished..."
