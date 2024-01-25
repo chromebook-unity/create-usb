1d525b18518ca0b7bd498ad713bced3354306378
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-01-24 21:03:57 -0800

    fix brave repo after upgrade

--- update-image.sh
+++ update-image.sh
@@ -16,12 +16,22 @@ do
             sudo sed -i 's/^\(Prompt=\).*$/\1lts/' /etc/update-manager/release-upgrades
             sudo do-release-upgrade
             sudo apt clean
+            echo "Enabling Brave Browser repository..."
+            sudo apt install curl -y
+            sudo rm -rf /etc/apt/sources.list.d/brave-browser-release.list*
+            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+            sudo apt update
             ;;
         "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)")
             echo "Upgrading to next devel LTS release (ex. 23.04, 23.10, etc.)"
             sudo sed -i 's/^\(Prompt=\).*$/\1normal/' /etc/update-manager/release-upgrades
             sudo do-release-upgrade
             sudo apt clean
+            echo "Enabling Brave Browser repository..."
+            sudo apt install curl -y
+            sudo rm -rf /etc/apt/sources.list.d/brave-browser-release.list*
+            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+            sudo apt update
             ;;
         "Quit")
             echo "Quitting..."
