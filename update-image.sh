389c778ff9ac4e66e8400380da709fd0f3922987
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-04-04 17:01:43 -0700

    Update update-image.sh

--- update-image.sh
+++ update-image.sh
@@ -6,38 +6,14 @@ echo "Upgrading packages.."
 sudo apt upgrade -y
 echo "Installing packages needed for upgrading..."
 sudo apt install update-manager-core -y
-PS3='What upgrade would you like to do? '
-options=("Upgrade to next LTS (ex. 22.04, etc.)" "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Upgrade to next LTS (ex. 22.04, etc.)")
-            echo "Upgrading to next LTS (ex. 22.04, etc.)"
-            sudo sed -i 's/^\(Prompt=\).*$/\1lts/' /etc/update-manager/release-upgrades
-            sudo do-release-upgrade
-            sudo apt clean
-            echo "Enabling Brave Browser repository..."
-            sudo apt install curl -y
-            sudo rm -rf /etc/apt/sources.list.d/brave-browser-release.list*
-            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-            sudo apt update
-            ;;
-        "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)")
-            echo "Upgrading to next devel LTS release (ex. 23.04, 23.10, etc.)"
-            sudo sed -i 's/^\(Prompt=\).*$/\1normal/' /etc/update-manager/release-upgrades
-            sudo do-release-upgrade
-            sudo apt clean
-            echo "Enabling Brave Browser repository..."
-            sudo apt install curl -y
-            sudo rm -rf /etc/apt/sources.list.d/brave-browser-release.list*
-            echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-            sudo apt update
-            ;;
-        "Quit")
-            echo "Quitting..."
-            exit
-            ;;
-        *) echo "Invalid option $REPLY";;
-    esac
-done
+
+OLD=$(sed 's/UBUNTU_CODENAME=//;t;d' /etc/os-release)
+read -p "Enter the Ubuntu Version you want to upgrade to. For example, mantic, for Ubuntu 23.10. (CASE-SENSITIVE - DO NOT PUT IN A FULL NAME): " NEW
+
+echo "The upgrading process has started!"
+
+sudo sed -i s/$OLD/$NEW/ /etc/apt/sources.list
+sudo apt-get update
+sudp apt dist-upgrade -y
+sudo apt autoremove -y
 
