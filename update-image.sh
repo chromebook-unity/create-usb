8b1d2d0336fdabb492869a0a3c5b7c1ea63526e7
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2024-04-09 09:53:30 -0700

    Update update-image.sh

--- update-image.sh
+++ update-image.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 echo "Installing packages needed for upgrading..."
 sudo apt install update-manager-core -y
 PS3='What upgrade would you like to do? '
-options=("Upgrade to next LTS (ex. 22.04, etc.)" "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)" "Upgrade to a development release (CAN BE BUGGY)" "Quit")
+options=("Upgrade to next LTS (ex. 22.04, etc.)" "Upgrade to a development release (CAN BE BUGGY)" "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -23,10 +23,10 @@ do
             sudo apt update
             echo "Type in 4 in the next prompt to exit."
             ;;
-        "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)")
-            echo "Upgrading to next devel LTS release (ex. 23.04, 23.10, etc.)"
-            sudo sed -i 's/^\(Prompt=\).*$/\1normal/' /etc/update-manager/release-upgrades
-            sudo do-release-upgrade
+       "Upgrade to the newest development release (CAN BE BUGGY)")
+            echo "Upgrading to the newest development release..."
+            echo "WARNING: USING A DEVELOPMENT RELEASE IS VERY BUGGY AND MAY CAUSE YOUR SYSTEM TO HAVE UNEXPECTED ERRORS. PROCEED WITH CAUTION!"
+            sudo do-release-upgrade -d
             sudo apt clean
             echo "Enabling Brave Browser repository..."
             sudo apt install curl -y
@@ -35,10 +35,10 @@ do
             sudo apt update
             echo "Type in 4 in the next prompt to exit."
             ;;
-       "Upgrade to the newest development release (CAN BE BUGGY)")
-            echo "Upgrading to the newest development release..."
-            echo "WARNING: USING A DEVELOPMENT RELEASE IS VERY BUGGY AND MAY CAUSE YOUR SYSTEM TO HAVE UNEXPECTED ERRORS. PROCEED WITH CAUTION!"
-            sudo do-release-upgrade -d
+        "Upgrade to next devel LTS release (ex. 23.04, 23.10, etc.)")
+            echo "Upgrading to next devel LTS release (ex. 23.04, 23.10, etc.)"
+            sudo sed -i 's/^\(Prompt=\).*$/\1normal/' /etc/update-manager/release-upgrades
+            sudo do-release-upgrade
             sudo apt clean
             echo "Enabling Brave Browser repository..."
             sudo apt install curl -y
