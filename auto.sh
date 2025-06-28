5aee96482a3146aab850d35266bb8ed5ad9d8c4a
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2025-06-28 14:41:30 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -8,7 +8,8 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 
 export LANG=C
-
+apt-get update
+apt-get -y install debootstrap btrfs-progs
 POSSIBLE_TARGET_HOST="aarch64"
 read -p "Choose platform (mt8183/mt8173): " mtk
 if [[ "$mtk" != "mt8183" && "$mtk" != "mt8173" ]]; then
@@ -85,19 +86,24 @@ cd ${BUILD_ROOT}/
 rm -rf post2.sh
 wget https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/6.12.28-stb-cbm%2B/6.12.28-stb-cbm+.tar.gz
 tar -xvf 6.12.28-stb-cbm+.tar.gz
+if [[ "$mtk" == "mt8183" ]]; then
+      wget https://github.com/velvet-os/imagebuilder/raw/refs/heads/main/systems/chromebook_kukui/rc-local-additions.txt
+      echo "" >> etc/rc.local
+      echo "# additions for ${1}" >> etc/rc.local
+      echo "" >> etc/rc.local
+      wget https://github.com/velvet-os/imagebuilder/raw/refs/heads/main/systems/chromebook_kukui/rc-local-additions.txt
+      cat rc-local-additions.txt >> etc/rc.local
+fi
 
-if [ -f ${WORKDIR}/systems/${1}/rc-local-additions.txt ]; then
-  echo "" >> etc/rc.local
-  echo "# additions for ${1}" >> etc/rc.local
-  echo "" >> etc/rc.local
-  cat ${WORKDIR}/systems/${1}/rc-local-additions.txt >> etc/rc.local
+if [[ "$mtk" == "mt8173" ]]; then
+      wget https://github.com/velvet-os/imagebuilder/raw/refs/heads/main/systems/chromebook_kukui/rc-local-additions.txt
+      echo "" >> etc/rc.local
+      echo "# additions for ${1}" >> etc/rc.local
+      echo "" >> etc/rc.local
+      wget https://github.com/velvet-os/imagebuilder/blob/main/systems/chromebook_oak/rc-local-additions.txt
+      cat rc-local-additions.txt >> etc/rc.local
 fi
-if [ -f ${WORKDIR}/systems/${1}/rc-local-additions-${3}.txt ]; then
-  echo "" >> etc/rc.local
-  echo "# additions for ${1} ${3}" >> etc/rc.local
-  echo "" >> etc/rc.local
-  cat ${WORKDIR}/systems/${1}/rc-local-additions-${3}.txt >> etc/rc.local
-fi
+
 echo "" >> etc/rc.local
 echo "exit 0" >> etc/rc.local
 
@@ -131,68 +137,27 @@ mkdir -p etc/systemd/system/multi-user.target.wants
 echo "uninitialized" > etc/machine-id
 rm -f var/lib/systemd/random-seed var/lib/dbus/machine-id
 
-# if a different default user name was set, parse it into the rename user script
-sed -i "s,DEFAULT_USERNAME=linux,DEFAULT_USERNAME=${DEFAULT_USERNAME},g" scripts/rename-default-user.sh
-
-# create an empty xorg.conf.d dir where the xorg config files can go to
 mkdir -p ${BUILD_ROOT}/etc/X11/xorg.conf.d
 
-# add support for self built fresher mesa
-if [ "${2}" = "armv7l" ]; then
-  echo "LD_LIBRARY_PATH=/opt/mesa/lib/arm-linux-gnueabihf/dri:/usr/lib/arm-linux-gnueabihf/dri" > etc/environment.d/50opt-mesa.conf
-  echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/arm-linux-gnueabihf/dri:/usr/lib/arm-linux-gnueabihf/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "GBM_DRIVERS_PATH=/opt/mesa/lib/arm-linux-gnueabihf/dri:/usr/lib/arm-linux-gnueabihf/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "/opt/mesa/lib/arm-linux-gnueabihf" > etc/ld.so.conf.d/aaa-mesa.conf
-elif [ "${2}" = "aarch64" ]; then
-  echo "LD_LIBRARY_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" > etc/environment.d/50opt-mesa.conf
-  echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "GBM_DRIVERS_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "/opt/mesa/lib/aarch64-linux-gnu" > etc/ld.so.conf.d/aaa-mesa.conf
-elif [ "${2}" = "i686" ]; then
-  echo "LD_LIBRARY_PATH=/opt/mesa/lib/i386-linux-gnu/dri:/usr/lib/i386-linux-gnu/dri" > etc/environment.d/50opt-mesa.conf
-  echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/i386-linux-gnu/dri:/usr/lib/i386-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "GBM_DRIVERS_PATH=/opt/mesa/lib/i386-linux-gnu/dri:/usr/lib/i386-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "/opt/mesa/lib/i386-linux-gnu" > etc/ld.so.conf.d/aaa-mesa.conf
-elif [ "${2}" = "x86_64" ]; then
-  echo "LD_LIBRARY_PATH=/opt/mesa/lib/x86_64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" > etc/environment.d/50opt-mesa.conf
-  echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/x86_64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "GBM_DRIVERS_PATH=/opt/mesa/lib/x86_64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "/opt/mesa/lib/x86_64-linux-gnu" > etc/ld.so.conf.d/aaa-mesa.conf
-elif [ "${2}" = "riscv64" ]; then
-  echo "LD_LIBRARY_PATH=/opt/mesa/lib/riscv64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" > etc/environment.d/50opt-mesa.conf
-  echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/riscv64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "GBM_DRIVERS_PATH=/opt/mesa/lib/riscv64-linux-gnu/dri:/usr/lib/x86_64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
-  echo "/opt/mesa/lib/riscv64-linux-gnu" > etc/ld.so.conf.d/aaa-mesa.conf
-fi
+echo "LD_LIBRARY_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" > etc/environment.d/50opt-mesa.conf
+echo "LIBGL_DRIVERS_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
+echo "GBM_DRIVERS_PATH=/opt/mesa/lib/aarch64-linux-gnu/dri:/usr/lib/aarch64-linux-gnu/dri" >> etc/environment.d/50opt-mesa.conf
+echo "/opt/mesa/lib/aarch64-linux-gnu" > etc/ld.so.conf.d/aaa-mesa.conf
 cp etc/environment.d/50opt-mesa.conf etc/X11/Xsession.d/50opt-mesa.conf
 echo "export LD_LIBRARY_PATH LIBGL_DRIVERS_PATH GBM_DRIVERS_PATH" >> etc/X11/Xsession.d/50opt-mesa.conf
 
-# add some imagebuilder version info as /etc/imagebuilder-info
-IMAGEBUILDER_VERSION=$(cd ${WORKDIR}; git rev-parse --verify HEAD)
-echo ${1} ${2} ${3} ${IMAGEBUILDER_VERSION} > ${BUILD_ROOT}/etc/imagebuilder-info
-
-# copy postinstall files into the build root if there are any
-if [ -d ${DOWNLOAD_DIR}/postinstall-${1} ]; then
-  cp -r ${DOWNLOAD_DIR}/postinstall-${1} ${BUILD_ROOT}/postinstall
+if [[ "$mtk" == "mt8173" ]]; then
+    cd ${BUILD_ROOT}
+    wget https://github.com/velvet-os/imagebuilder/raw/refs/heads/main/systems/chromebook_oak/postinstall.sh 
 fi
 
-# post install script per system
-if [ -f ${WORKDIR}/systems/${1}/postinstall.sh ]; then
-  bash ${WORKDIR}/systems/${1}/postinstall.sh ${1} ${2} ${3}
+if [[ "$mtk" == "mt8183" ]]; then
+    cd ${BUILD_ROOT}
+    wget https://github.com/velvet-os/imagebuilder/raw/refs/heads/main/systems/chromebook_kukui/postinstall.sh
 fi
 
-# post install script which is run chrooted per system
-if [ -f ${WORKDIR}/systems/${1}/postinstall-chroot.sh ]; then
-  cp ${WORKDIR}/systems/${1}/postinstall-chroot.sh ${BUILD_ROOT}/postinstall-chroot.sh
-  chmod a+x ${BUILD_ROOT}/postinstall-chroot.sh
-  chroot ${BUILD_ROOT} /postinstall-chroot.sh ${1} ${2} ${3}
-  rm -f ${BUILD_ROOT}/postinstall-chroot.sh
-fi
-
-# cleanup postinstall files
-if [ -d ${BUILD_ROOT}/postinstall ]; then
-  rm -rf ${BUILD_ROOT}/postinstall
-fi
+chroot ${BUILD_ROOT} /postinstall-chroot.sh
+rm -rf ${BUILD_ROOT}/postinstall-chroot.sh
 
 # recompile glib schemas to enable our onboard settings
 chroot ${BUILD_ROOT} glib-compile-schemas /usr/share/glib-2.0/schemas/
@@ -204,35 +169,8 @@ chroot ${BUILD_ROOT} apt-get -y clean
 
 chroot ${BUILD_ROOT} ldconfig
 
-if [ "${PMOSKERNEL}" != "true" ]; then
-  export KERNEL_VERSION=`ls ${BUILD_ROOT}/boot/*Image-* | sed 's,.*Image-,,g' | sort -u`
-
-  # in case we did not get a kernel version, try it again with the vmlinuz
-  if [ "$KERNEL_VERSION" = "" ]; then
-    echo "trying vmlinuz as kernel name instead of *Image:"
-    export KERNEL_VERSION=`ls ${BUILD_ROOT}/boot/vmlinuz-* | sed 's,.*vmlinuz-,,g' | sort -u`
-  fi
-
-  if [ "$KERNEL_VERSION" = "" ]; then
-    echo "no KERNEL_VERSION - lets assume this is intended and ignore the initramfs rebuild"
-  else
-    # hack to get the fsck binaries in properly even in our chroot env
-    cp -f usr/share/initramfs-tools/hooks/fsck tmp/fsck.org
-    sed -i 's,fsck_types=.*,fsck_types="vfat ext4",g' usr/share/initramfs-tools/hooks/fsck
-    chroot ${BUILD_ROOT} update-initramfs -c -k ${KERNEL_VERSION}
-    mv -f tmp/fsck.org usr/share/initramfs-tools/hooks/fsck
-  fi
-else
-  # the pmos boot.img reads its initrd extension from here (if not in boot-and-modules.tar.gz)
-  if [ -f boot/extra/initramfs-extra ]; then
-    cp boot/extra/initramfs-extra boot
-  fi
-fi
-
 cd ${WORKDIR}
 
 umount ${BUILD_ROOT}/proc ${BUILD_ROOT}/sys ${BUILD_ROOT}/dev/pts ${BUILD_ROOT}/dev
 
-echo ""
-echo "now run create-image.sh ${1} ${2} ${3} to build the image"
-echo ""
+
